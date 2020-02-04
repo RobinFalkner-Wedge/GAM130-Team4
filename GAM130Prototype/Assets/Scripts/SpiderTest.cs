cadfae78eb416a0a4c2955c1022ad7e58e54bf39
robin <rf231021@falmouth.ac.uk>
2020-02-04 14:37:56 +0000

    made a scene with a nav mesh for making the spider on.
    I have no idea how to get the spider working, though.

---
+++ GAM130Prototype/Assets/Scripts/SpiderTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderTest : MonoBehaviour
+{
+
+    private bool isChase;
+    private bool isHunt;
+    private bool isPatroling;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 raycastDir = thing.transform.position - transform.position;
+
+        isChase = Physics.Raycast(transform.position, Vector3.down, 2.5f);
+
+
+
+
+
+
+    }
+}
